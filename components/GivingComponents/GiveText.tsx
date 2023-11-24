551c5e8ef203323fa61fc916f76c68004471b42c
kofia894 <60142082+kofia894@users.noreply.github.com>
2023-11-24 21:05:11 +0000

    Upate Border radius responsivenes

--- components/GivingComponents/GiveText.tsx
+++ components/GivingComponents/GiveText.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function GiveText() {
   return (
-    <div className="rounded-b-[85px] bg-primary pt-40 w-screen min-h-screen flex flex-col justify-center items-center">
+    <div className="rounded-b-[45px] md:rounded-b-[85px] bg-primary pt-40 w-screen min-h-screen flex flex-col justify-center items-center">
       <h1 className="font-Barlow text-white text-center lg:text-7xl md:text-5xl text-4xl w-full ">Thank you for Investing </h1>
       <h1 className="font-Barlow text-white text-center lg:text-7xl md:text-5xl text-4xl w-full mb-20" >into the Kingdom of God</h1>
       <h1 className="text-center text-white font-base leading-loose md:text-2xl text-xl italic lg:mx-60 mx-10 md:leading-loose" data-aos="fade-up" data-aos-duration="1000" >"Faithfulness in giving offerings and paying tithes to enhance the ministry of the Church is emphasised. The Church and members depend solely on God as the source of financial supply"</h1>
